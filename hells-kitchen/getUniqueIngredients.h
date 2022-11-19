b5e0002e4010080cb50cf45039fbd25e5594e1f7
Emily Nham <emilynham@Emilys-MacBook-Air.local>
2022-11-19 15:30:32 -0800

    Round 1

--- hells-kitchen/getUniqueIngredients.h
+++ hells-kitchen/getUniqueIngredients.h
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <set>
 using namespace std;
 
 int getUniqueIngredients(string ingredients[], int size) {
-    return 0;
+    std::set<string> processed;
+    string temp;
+    
+    for(int i = 0; i < size; i++)
+    {
+        temp = "";
+        for (int j = 0; j < ingredients[i].size(); j++)
+        {
+            if (isalpha(ingredients[i].at(j)))
+                temp += tolower(ingredients[i].at(j));
+        }
+        processed.insert(temp);
+    }
+
+    
+    return processed.size();
 }
--- hells-kitchen/testGetUniqueIngredients.cpp
+++ hells-kitchen/testGetUniqueIngredients.cpp
@@ -10,4 +10,25 @@ int main() {
     } else {
         cout << "Failed test case :(" << endl;
     }
+
+    {string ingredients2[] = {"ch.ick.en wi.n.g.s", "onion@rings000", "chickenwings", "oNioNriNgs"};
+    if (getUniqueIngredients(ingredients2, 4) == 2) {
+        cout << "Passed" << endl;
+    } else {
+        cout << "Failed" << endl;
+    }}
+
+        {string ingredients2[] = {"cheese", "cheEse", "CHEESE", "cheese0", "_cheese_", "c-h-e-e-s-e", "c0h0e0e0s0e", "c heese0"};
+    if (getUniqueIngredients(ingredients2, 8) == 1) {
+        cout << "Passed" << endl;
+    } else {
+        cout << "Failed" << endl;
+    }}
+
+        {string ingredients3[] = {};
+    if (getUniqueIngredients(ingredients3, 0) == 0) {
+        cout << "Passed" << endl;
+    } else {
+        cout << "Failed" << endl;
+    }}
 }
